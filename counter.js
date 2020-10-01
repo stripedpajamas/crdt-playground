8b9de4bf3e758a134cb172e9d06d0b1976337bba
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-09-30 17:03:39 -0700

    Add comments

--- counter.js
+++ counter.js
@@ -1,13 +1,19 @@
+// One of the most basic CRDTs; this Counter is state-based,
+// meaning that the entire state is sent to other nodes when
+// merging.
 class Counter {
   constructor (id) {
+    // state holds all the nodes' values, keyed by id
     this.state = []
     this.id = id
   }
 
+  // inc increments this node's value in this node's state
   inc () {
     this.state[this.id] = (this.state[this.id] || 0) + 1
   }
 
+  // value returns the sum of all nodes' values according to this node
   value () {
     return this.state.reduce((acc, el) => acc + el, 0)
   }
@@ -16,6 +22,8 @@ class Counter {
     return this.state
   }
 
+  // merge sets this node's state to the maximum value of each
+  // node between local state and other state
   merge (otherState) {
     otherState.forEach((node, idx) => {
       if (typeof this.state[idx] === 'undefined') {
